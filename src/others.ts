f011499b7c42eaec13e67162805febe3734f062d
smabari <mdazizulbarishafin@gmail.com>
2025-09-06 09:42:24 +0600

    feat: update dev script to point to others.ts and enhance others.ts with additional type examples and nullish policy

--- src/others.ts
+++ src/others.ts
@@ -13,6 +13,53 @@
     bondhu: "Upokari",
     shotru: 2,
   };
+
+  // ternary
+  // let age = 21;
+  // const status = age >= 18 ? "Voter" : "Shishu";
+  let age = "test";
+
+  if (typeof age == "number") {
+    type voter = number;
+  }
+
+  type VoterType = typeof age extends number ? number : string;
+
+  // console.log(status);
+
+  // optional chaining
+
+  interface Iuser {
+    name: string;
+    age: number;
+    address: {
+      country: string;
+      city: string;
+    };
+  }
+
+  const user = {
+    name: "jekono",
+    age: 32,
+    address: {
+      country: "BD",
+      city: "Dhaka",
+    },
+  };
+  console.log(user?.address?.city);
 }
 
-// 8 min 
+// nullish policy
+const input = null;
+const userInput = input ?? "GuestMajor ";
+
+// never type
+function crash(): never {
+  //you can also use void too
+  throw new Error("Error hoye gese vhai");
+}
+// crash();
+
+// unknown type
+let nm: unknown = "Tanjim";
+console.log((nm as string).length);
