e3e969f54a0e4167a1f48e38e76c1c39e6b37236
smabari <mdazizulbarishafin@gmail.com>
2025-09-07 00:59:16 +0600

    feat: add asynchronous functions and promise handling examples in secondhalf.ts

--- src/secondhalf.ts
+++ src/secondhalf.ts
@@ -60,4 +60,48 @@
   }
   const name = getProperty(user23, "name");
   console.log(typeof name);
+
+  // asynchronus TS
+  async function fetchUser(): Promise<string> {
+    return "str";
+  }
+
+  const myPromise = new Promise((resolve, reject) => {
+    const success = false;
+
+    setTimeout(() => {
+      if (success) {
+        resolve("promise resolved successfully");
+      } else {
+        reject("promise rejected due to an error");
+      }
+    }, 1000);
+  });
+
+  myPromise
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
+
+  console.log(fetchUser());
+  fetchUser().then((user) => console.log(user));
+
+  // extisting map
+
+  // interface IUser {
+  //   name: string;
+  //   age: number;
+  // }
+
+  // interface ILearner extends IUser {
+  //   attitude: "learning";
+  // }
+
+  type result<T> = {
+    [J in keyof T]: boolean;
+  };
+  const resultTest: result<ILearner> = {
+    name: true,
+    age: true,
+    attitude: true,
+  };
 }
